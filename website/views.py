0eef19b2240634a0a786c2dfa95acae9d4e540cf
avalasai <avalasai@gmail.com>
2023-11-19 02:09:12 -0800

    Added Delete Page

--- website/views.py
+++ website/views.py
@@ -41,6 +41,26 @@ def customer_record(request , pk):
         messages.success(request, "You must be logged in to view the Contacts Page")
         return redirect('home')
 
+def delete_page(request ,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'delete.html',{'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view the delete page")
+        return redirect('home')
+
+
+
+def delete_record(request,pk):
+    if request.user.is_authenticated:
+        delete_rec = Record.objects.get(id=pk)
+        delete_rec.delete()
+        messages.success(request,"You have Deleted the recorded")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to delete the record")
+        return redirect('home')
+        
 
 
 
